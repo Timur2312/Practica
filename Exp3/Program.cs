6ec1925c4f17b6279193e0e2642a388e052d1322
Timur <arbo1234@yandex.ru>
2022-11-04 14:43:02 +0300

    Добавил задание 2 и 3

---
+++ Exp3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * сортировка массива 
+ */
+int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
+int temp;
+for (int i = 0; i < nums.Length; i++)
+{
+    for (int j = i+1; j < nums.Length; j++)
+    {
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"{nums[i]} ");
+}
+Console.ReadLine();
